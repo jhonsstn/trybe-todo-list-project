3913c75cea43cb85858882d91239cace03edfee9
Jhon Santana <jhonsstn@gmail.com>
2021-12-19 20:06:32 -0300

    completados items de 1 a 9 e realizados ajustes

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
-let taskList = document.querySelector('#lista-tarefas');
+const htmlTaskList = document.querySelector('#lista-tarefas');
 
-document.getElementById('criar-tarefa').addEventListener('click', () => {
-  let inputValue = document.getElementById('texto-tarefa').value;
-  createAndAddItem(inputValue);
-  document.getElementById('texto-tarefa').value = '';
-});
+function grayBgColor(element, index, array) {
+  element.addEventListener('click', (e) => {
+    for (let i = 0; i < array.length; i += 1) {
+      const arrayItem = array[i];
+      arrayItem.style.backgroundColor = '';
+    }
+    e.target.style.backgroundColor = 'gray';
+  });
+}
+
+function lineThrough(e) {
+  if (e.target.classList.contains('completed')) {
+    e.target.classList.remove('completed');
+  } else {
+    e.target.classList.add('completed');
+  }
+}
 
 function createAndAddItem(value) {
-  let taskElement = document.createElement('li');
+  const taskElement = document.createElement('li');
   taskElement.innerText = value;
-  taskList.append(taskElement);
+  taskElement.className = 'todo-item';
+  taskElement.addEventListener('dblclick', lineThrough);
+  htmlTaskList.append(taskElement);
 }
+
+document.getElementById('criar-tarefa').addEventListener('click', () => {
+  const inputValue = document.getElementById('texto-tarefa').value;
+  createAndAddItem(inputValue);
+  document.getElementById('texto-tarefa').value = '';
+
+  const nodeTaskList = document.querySelectorAll('.todo-item');
+
+  nodeTaskList.forEach(grayBgColor);
+});
